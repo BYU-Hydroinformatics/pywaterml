6ab7088386d8a5dd2de35991994c4010b8ff28de
Elkin Romero <gio.busrom@gmail.com>
2020-10-28 16:44:14 -0600

    addition of the GetSiteInfo function to retireve data from a specific site the return object is an array of the different variables that it presents

--- operations.py
+++ operations.py
@@ -7,19 +7,22 @@ from pyproj import Proj, transform  # Reprojecting/Transforming coordinates
 import xml.etree.ElementTree as ET
 
 class WaterMLOperations(url):
+    def __init__(self,url = None):
+        self.url = url
+        self.client = Client(url, timeout= 500)
 
+    # client = Client(url, timeout= 500)
     """
         Get all the sites from a endpoint
         GetSites() function is similar to the
         GetSites() WaterML function
     """
-    client = Client(url, timeout= 500)
 
     def GetSites():
         # True Extent is on and necessary if the user is trying to add USGS or
         # Get a list of all the sites and their respective lat lon.
-        
-        sites = client.service.GetSites('[:]')
+
+        sites = self.client.service.GetSites('[:]')
         sites_json={}
         if isinstance(sites, str):
             sites_dict = xmltodict.parse(sites)
@@ -47,7 +50,7 @@ class WaterMLOperations(url):
         maxx, maxy = ext_list[2], ext_list[3]
         x1, y1 = transform(inProj, outProj, minx, miny)
         x2, y2 = transform(inProj, outProj, maxx, maxy)
-        bbox = client.service.GetSitesByBoxObject(
+        bbox = self.client.service.GetSitesByBoxObject(
             x1, y1, x2, y2, '1', '')
         # Get Sites by bounding box using suds
         # Creating a sites object from the endpoint. This site object will
@@ -58,12 +61,12 @@ class WaterMLOperations(url):
 
         return sites_parsed_json
     """
-        Get all the sites from a selected biding box
-        GetSitesByBoxObject() function is similar to the
-        GetSitesByBoxObject() WaterML function
+        Get all the variables from an endpoint
+        GetVariables() function is similar to the
+        GetVariables() WaterML function
     """
     def GetVariables():
-      variables = client.service.GetVariables('[:]')
+      variables = self.client.service.GetVariables('[:]')
 
       variables_dict = xmltodict.parse(keywords)
       variables_dict_object = json.dumps(keywords_dict)
@@ -80,6 +83,50 @@ class WaterMLOperations(url):
           array_final_variables.append(array_variables['variableName'])
 
       return array_final_variables
+    """
+        Get the information from a site.
+        GetSiteInfo() function is similar to the
+        GetSiteInfo() WaterML function
+    """
+    def GetSiteInfo(site_ful_code):
+
+        site_info_Mc = self.client.service.GetSiteInfo(site_full_code)
+        site_info_Mc_dict = xmltodict.parse(site_info_Mc)
+        site_info_Mc_json_object = json.dumps(site_info_Mc_dict)
+        site_info_Mc_json = json.loads(site_info_Mc_json_object)
+
+        object_methods = site_info_Mc_json['sitesResponse']['site']['seriesCatalog']['series']
+
+        return_aray = []
+        if(isinstance(object_methods,(dict))):
+            return_obj = {}
+            return_obj['name'] = object_methods['variable']['variableName']
+            return_objec['code'] = object_methods['variable']['variableCode']['#text']
+            return_objec['count'] = object_methods['valueCount']
+            if 'method' in object_methods:
+                return_object['methodID'] = object_methods['method']['@methodID']
+            else:
+                return_object['methodID'] = None
+            return_obj['description'] = object_methods['source']
+            return_obj['timeInterval'] = object_methods['variableTimeInterval']
+            return_aray.append(return_obj)
+            return return_aray
+
+        else:
+            for object_method in object_methods:
+                return_obj = {}
+                return_obj['name'] = object_method['variable']['variableName']
+                return_objec['code'] = object_method['variable']['variableCode']['#text']
+                return_objec['count'] = object_method['valueCount']
+                if 'method' in object_method:
+                    return_object['methodID'] = object_method['method']['@methodID']
+                else:
+                    return_object['methodID'] = None
+                return_obj['description'] = object_method['source']
+                return_obj['timeInterval'] = object_method['variableTimeInterval']
+                return_aray.append(return_obj)
+
+        return return_aray
 
 if __name__ == "__main__":
     print("WaterML ops")
