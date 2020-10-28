d41eebeee1a850b2f271bf717d01811c3d2cf9f8
Elkin Romero <gio.busrom@gmail.com>
2020-10-28 17:12:10 -0600

    addition of the GetSiteInfo and MeanInterpolation functions to provide the user the values of a variable in a specific site and a mean interpolation if values will be needed

--- operations.py
+++ operations.py
@@ -5,6 +5,8 @@ from json import dumps, loads
 from aux import Auxiliary
 from pyproj import Proj, transform  # Reprojecting/Transforming coordinates
 import xml.etree.ElementTree as ET
+import pandas as pd
+
 
 class WaterMLOperations(url):
     def __init__(self,url = None):
@@ -127,6 +129,148 @@ class WaterMLOperations(url):
                 return_aray.append(return_obj)
 
         return return_aray
+    """
+        Get the specific site for an especific variable in a site.
+        GetValues() function is similar to the
+        GetValues() WaterML function
+    """
+    def GetValues(site_full_code, variable_full_code, methodID, start_date, end_date):
+
+        values = self.client.service.GetValues(
+            site_full_code, variable_full_code, start_date, end_date, "")
+        values_dict = xmltodict.parse(values)  # Converting xml to dict
+        values_json_object = json.dumps(values_dict)
+        values_json = json.loads(values_json_object)
+        times_series = {}
+        if 'timeSeriesResponse' in values_json:
+
+            times_series = values_json['timeSeriesResponse'][
+                'timeSeries']  # Timeseries object for the variable
+            if times_series['values'] is not None:
+
+                graph_json = {}  # json object that will be returned to the front end
+                graph_json["variable"] = times_series['variable']['variableName']
+                graph_json["unit"]=""
+                if times_series['variable']['unit']['unitAbbreviation'] is not None:
+                    graph_json["unit"] = times_series[
+                        'variable']['unit']['unitAbbreviation']
+
+                graph_json["title"] = times_series['variable']['variableName'] + " (" + graph_json["unit"] + ") vs Time"
+                for j in times_series['values']:  # Parsing the timeseries
+                    data_values = []
+                    if j == "value":
+                        if type(times_series['values']['value']) is list:
+
+                            for k in times_series['values']['value']:
+                                return_obj['k']= k
+
+                                try:
+                                    if k['@methodCode'] == methodID:
+                                        time = k['@dateTimeUTC']
+                                        time1 = time.replace("T", "-")
+                                        time_split = time1.split("-")
+                                        year = int(time_split[0])
+                                        month = int(time_split[1])
+                                        day = int(time_split[2])
+                                        hour_minute = time_split[3].split(":")
+                                        hour = int(hour_minute[0])
+                                        minute = int(hour_minute[1])
+                                        value = float(str(k['#text']))
+                                        date_string = datetime(
+                                            year, month, day, hour, minute)
+                                        date_string_converted = date_string.strftime("%Y-%m-%d %H:%M:%S")
+                                        data_values.append([date_string_converted,value])
+                                        data_values.sort()
+                                    graph_json["values"] = data_values
+                                except KeyError:  # The Key Error kicks in when there is only one timeseries
+                                    time = k['@dateTimeUTC']
+                                    time1 = time.replace("T", "-")
+                                    time_split = time1.split("-")
+                                    year = int(time_split[0])
+                                    month = int(time_split[1])
+                                    day = int(time_split[2])
+                                    hour_minute = time_split[3].split(":")
+                                    hour = int(hour_minute[0])
+                                    minute = int(hour_minute[1])
+                                    value = float(str(k['#text']))
+                                    date_string = datetime(
+                                        year, month, day, hour, minute)
+                                    data_values.append([date_string,value])
+                                    data_values.sort()
+                                graph_json["values"] = data_values
+                                return_obj['graphs']= graph_json
+
+                        else:  # The else statement is executed is there is only one value in the timeseries
+                            try:
+                                if times_series['values']['value']['@methodCode'] == methodID:
+                                    time = times_series['values'][
+                                        'value']['@dateTimeUTC']
+                                    time1 = time.replace("T", "-")
+                                    time_split = time1.split("-")
+                                    year = int(time_split[0])
+                                    month = int(time_split[1])
+                                    day = int(time_split[2])
+                                    hour_minute = time_split[3].split(":")
+                                    hour = int(hour_minute[0])
+                                    minute = int(hour_minute[1])
+                                    value = float(
+                                        str(times_series['values']['value']['#text']))
+
+                                    date_string = datetime(
+                                        year, month, day, hour, minute)
+
+                                    data_values.append([date_string,value])
+                                    data_values.sort()
+                                    graph_json["values"] = data_values
+                                    return_obj['graphs']=graph_json
+
+                            except KeyError:
+                                time = times_series['values'][
+                                    'value']['@dateTimeUTC']
+                                time1 = time.replace("T", "-")
+                                time_split = time1.split("-")
+                                year = int(time_split[0])
+                                month = int(time_split[1])
+                                day = int(time_split[2])
+                                hour_minute = time_split[3].split(":")
+                                hour = int(hour_minute[0])
+                                minute = int(hour_minute[1])
+                                value = float(
+                                    str(times_series['values']['value']['#text']))
+                                date_string = datetime(
+                                    year, month, day, hour, minute)
+                                data_values.append([date_string,value])
+                                data_values.sort()
+                                graph_json["values"] = data_values
+                                return_obj['graphs']=graph_json
+        return return_obj
+    """
+        Get the mean interpolation of the values for an specific site
+        MeanInterpolation() function is a complement to the
+        GetValues() WaterML function if there is gaps in the data
+    """
+    def MeanInterpolation(GetValuesResponse):
+        time_pd, values_pd = zip(*GetValuesResponse)
+        pds={}
+        pds['time'] = time_pd
+        pds['value'] = values_pd
+        df_interpolation= pd.DataFrame(pds,columns = ["time","value"])
+        df_interpolation2= pd.DataFrame(pds,columns = ["time","value"])
+        df_interpolation.loc[df_interpolation.value < 0] = np.NaN
+        df_interpolation.replace(0, np.NaN, inplace=True)
+        df_interpolation['time'] = pd.to_datetime(df_interpolation['time'])
+        df_interpolation = df_interpolation.set_index('time').resample('D').mean()
+        df_interpolation['value'] = df_interpolation['value'].interpolate()
+        df_interpolation.reset_index(level=0, inplace=True)
+        df_interpolation.replace(np.NaN,0, inplace=True)
+        listVals = df_interpolation['value'].to_list()
+        listTimes = df_interpolation['time'].to_list()
+        dataInterpolated = []
+        #a count for the number of interpolated can be introduced
+        for t,v in zip(listTimes,listVals):
+            dataInterpolated.append([t,v])
+
+        return dataInterpolated
 
 if __name__ == "__main__":
     print("WaterML ops")
