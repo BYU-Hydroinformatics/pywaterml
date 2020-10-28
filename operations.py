bbacfd60854d9b4f7f93887af6dd66ac9f564b4f
Elkin Romero <gio.busrom@gmail.com>
2020-10-28 15:25:19 -0600

    the client is now a global variable, in addition, the GetVariables function was added to retrieve all the variables from a endpoint

--- operations.py
+++ operations.py
@@ -1,20 +1,24 @@
 from suds.client import Client  # For parsing WaterML/XML
 import json
+import xmltodict
 from json import dumps, loads
 from aux import Auxiliary
 from pyproj import Proj, transform  # Reprojecting/Transforming coordinates
+import xml.etree.ElementTree as ET
 
-class WaterMLOperations():
+class WaterMLOperations(url):
 
     """
         Get all the sites from a endpoint
         GetSites() function is similar to the
         GetSites() WaterML function
     """
-    def GetSites(url):
-        client = Client(url, timeout= 500)
+    client = Client(url, timeout= 500)
+
+    def GetSites():
         # True Extent is on and necessary if the user is trying to add USGS or
         # Get a list of all the sites and their respective lat lon.
+        
         sites = client.service.GetSites('[:]')
         sites_json={}
         if isinstance(sites, str):
@@ -34,9 +38,7 @@ class WaterMLOperations():
         GetSitesByBoxObject() function is similar to the
         GetSitesByBoxObject() WaterML function
     """
-    def GetSitesByBoxObject(url, ext_list):
-        client = Client(url, timeout= 500)
-        extent_value = request.POST['extent_val']
+    def GetSitesByBoxObject(ext_list):
         return_obj['level'] = extent_value
         # Reprojecting the coordinates from 3857 to 4326 using pyproj
         inProj = Proj(init='epsg:3857')
@@ -55,6 +57,29 @@ class WaterMLOperations():
         sites_parsed_json = json.dumps(wml_sites)
 
         return sites_parsed_json
+    """
+        Get all the sites from a selected biding box
+        GetSitesByBoxObject() function is similar to the
+        GetSitesByBoxObject() WaterML function
+    """
+    def GetVariables():
+      variables = client.service.GetVariables('[:]')
+
+      variables_dict = xmltodict.parse(keywords)
+      variables_dict_object = json.dumps(keywords_dict)
+
+      variables_json = json.loads(variables_dict_object)
+      array_variables = variables_json['variablesResponse']['variables']['variable']
+      array_final_variables = []
+
+      if isinstance(array_variables,type([])):
+          for one_variable in array_variables:
+              array_final_variables.append(one_variable['variableName'])
+
+      if isinstance(array_variables,dict):
+          array_final_variables.append(array_variables['variableName'])
+
+      return array_final_variables
 
 if __name__ == "__main__":
     print("WaterML ops")
