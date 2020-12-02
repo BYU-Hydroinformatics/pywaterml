ac7479978bc4a07afe52aa69cbd7fbf2966d202b
Elkin Romero <gio.busrom@gmail.com>
2020-12-01 23:56:42 -0700

    MonthlyAverages() and MonthlyAverageCluster() functions added to the analyzeData.

--- pywaterml/analyzeData.py
+++ pywaterml/analyzeData.py
@@ -39,5 +39,48 @@ class WaterAnalityca():
 
         return dataInterpolated
 
+    def MonthlyAverages(GetValuesResponse):
+        columns = ['dates','values']
+        df = pd.DataFrame(GetValuesResponse['values'], columns=columns)
+        df['dates'] = pd.to_datetime(df['dates'])
+        df_2 = df.groupby(df.dates.dt.strftime('%m')).values.agg(['mean'])
+        # df_2 = df.groupby(df.dates.dt.strftime('%Y-%m')).values.agg(['mean'])
+        # print(df_2)
+        m_avg = df_2.to_numpy()
+        m_avg = m_avg.reshape((m_avg.shape[0],))
+        m_avg = m_avg.tolist()
+        return m_avg
+
+    def dailyAverages(variable_site,CookieCutter = None):
+        d_avg = []
+        return d_avg
+
+    def MonthlyAverageClusters(Sites, Variable):
+        timeseries = []
+        i = 0
+        for site in Sites:
+            print(i)
+            site_full_code = f'{site["network"]}:{site["sitecode"]}'
+            siteInfo =  water.GetSiteInfo(site_full_code)
+            for sinfo in siteInfo:
+                if sinfo['name'] == Variable:
+                    print(site['name'])
+                    firstVariableCode = siteInfo[0]['code']
+                    variable_full_code = network + ":" + firstVariableCode
+                    methodID = siteInfo[0]['methodID']
+                    start_date = siteInfo[0]['timeInterval']['beginDateTime'].split('T')[0]
+                    end_date = siteInfo[0]['timeInterval']['endDateTime'].split('T')[0]
+                    variableResponse = water.GetValues(site_full_code, variable_full_code, methodID, start_date, end_date)
+                    m_avg = water.getMonthlyAverage(variableResponse)
+                    timeseries.append(to_time_series(m_avg))
+                    break
+            i = i + 1    
+        # X = np.array(np.array(list2))
+        formatted_time_series = to_time_series_dataset(timeseries)
+        # print(X)
+        model = TimeSeriesKMeans(n_clusters=2, metric="dtw", max_iter=10)
+        y_pred = model.fit_predict(formatted_time_series)
+        return y_pred
+
 if __name__ == "__main__":
     print("Why are you running the wrapper class file?")
