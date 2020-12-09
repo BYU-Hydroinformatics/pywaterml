d44342d6cc162732561046ef588bb620ce2ffd97
Elkin Romero <gio.busrom@gmail.com>
2020-12-09 16:10:45 -0700

    Update the version of from 0.0.13.dev to 0.0.14.dev

--- pywaterml/__init__.py
+++ pywaterml/__init__.py
@@ -2,5 +2,5 @@ import pywaterml.aux
 import pywaterml.analyzeData
 import pywaterml.waterML
 
-__version__ = '0.0.13.dev1'
+__version__ = '0.0.14.dev1'
 __author__ = 'Elkin Giovanni Romero Bustamante'
