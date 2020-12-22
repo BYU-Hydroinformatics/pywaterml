d367437804e61e146d1c318adc4b85ffe20a9d95
Elkin Romero <gio.busrom@gmail.com>
2020-12-21 20:20:04 -0700

    Change in versions

--- pywaterml/__init__.py
+++ pywaterml/__init__.py
@@ -2,5 +2,6 @@ import pywaterml.aux
 import pywaterml.analyzeData
 import pywaterml.waterML
 
-__version__ = '0.0.14.dev1'
+# __version__ = '0.0.14.dev1'
+__version__ = '1.1.1'
 __author__ = 'Elkin Giovanni Romero Bustamante'
