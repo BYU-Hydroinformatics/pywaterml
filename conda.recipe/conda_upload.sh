44c1a03064e7d884e7f9628c80a2489d1cecb84a
Elkin Romero <gio.busrom@gmail.com>
2020-12-24 14:09:25 -0700

    adding conda_upload.sh

---
+++ conda.recipe/conda_upload.sh
@@ -0,0 +1,13 @@
+USER=elkingio
+
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+ls -l
+conda build .
+
+find $CONDA_BLD_PATH/ -name *.tar.bz2 | while read file
+do
+    echo $file
+    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $file --force
+done
