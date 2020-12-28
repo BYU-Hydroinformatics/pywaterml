85e1b8cfa8f280cbc5bdfb2568b2a4b2dd2face1
Elkin Romero <gio.busrom@gmail.com>
2020-12-28 11:44:51 -0700

    added scripts for anaconda upload

--- conda.recipe/deploy_anaconda.sh
+++ conda.recipe/deploy_anaconda.sh
@@ -6,7 +6,7 @@
 set -e
 
 echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/$PKG_NAME-$PKG_VERSION-$PKG_BUILD_STRING*.tar.bz2 --output-dir conda-bld/
+conda convert --platform all $HOME/envs/test-environment/conda-bld/linux-64/$PKG_NAME-$PKG_VERSION-$PKG_BUILD_STRING*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
 anaconda -t $ANACONDA_TOKEN upload conda-bld/**/$PKG_NAME-$PKG_VERSION-$PKG_BUILD_STRING*.tar.bz2
