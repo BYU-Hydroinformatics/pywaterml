b454bc0e7bcf5d9483a5ed3dcb85e8287598b8fd
Elkin Romero <gio.busrom@gmail.com>
2020-12-28 12:01:30 -0700

    export some variables from meta.yml to deply_anaconda.sh

--- conda.recipe/deploy_anaconda.sh
+++ conda.recipe/deploy_anaconda.sh
@@ -4,7 +4,9 @@
 # >>> anaconda login
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
 set -e
-
+export PKG_NAME
+export PKG_VERSION
+export PKG_BUILD_STRING
 echo "Converting conda package..."
 conda convert --platform all $HOME/envs/test-environment/conda-bld/linux-64/$PKG_NAME-$PKG_VERSION-$PKG_BUILD_STRING*.tar.bz2 --output-dir conda-bld/
 
