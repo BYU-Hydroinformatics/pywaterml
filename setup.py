54590c9dc6941bfc90b642177d1b6ca5b4cd8a0c
romer8 <gio.busrom@gmail.com>
2023-08-03 15:59:56 -0600

    added

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+import versioneer
+
+# with open("README.md", "r") as readme:
+#     long_description = readme.read()
+#
+# with open('requirements.txt', 'r') as req:
+#     requirements = req.read().splitlines()
+
+requirements = [
+    "xmltodict",
+    "pandas",
+    "numpy",
+    "owslib",
+    "suds-community",
+    "tslearn", #==0.5.2
+    "numba" #0.55.1
+]
+
+setup(
+    name='pywaterml',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="The pywaterml is a package that lets you handle WaterML functions such as GetValues, GetSitesInfo, etc. In addition it lets offers extra functions such as mean interpolation for data with gaps",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='BSD 3-Clause',
+    license_family='BSD',
+    author="Elkin Giovanni Romero Bustamante",
+    author_email='gio.rombus@gmail.com',
+    url='https://github.com/BYU-Hydroinformatics/pywaterml',
+    packages=['pywaterml'],
+    install_requires=requirements,
+    keywords='WaterML',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Hydrology',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+    ]
+)
