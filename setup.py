b586d78908099866acc42b98f37f3e0b6d993a89
romer8 <gio.busrom@gmail.com>
2023-08-03 16:23:24 -0600

    zzz

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ requirements = [
 
 setup(
     name='pywaterml',
-    version='1.2.22',
-    cmdclass=versioneer.get_cmdclass(),
+    version='1.2.21',
+    # cmdclass=versioneer.get_cmdclass(),
     description="The pywaterml is a package that lets you handle WaterML functions such as GetValues, GetSitesInfo, etc. In addition it lets offers extra functions such as mean interpolation for data with gaps",
     long_description=long_description,
     long_description_content_type='text/markdown',
